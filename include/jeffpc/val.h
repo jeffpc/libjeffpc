f7983e154afcdf52a56f34f3069d76475f8ac55c
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2016-03-26 17:20:07 -0400

    val: a generic typed value structure
    The implementation currenly relies on libumem for slab allocation.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

---
+++ include/jeffpc/val.h
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2014-2016 Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __JEFFPC_VAL_H
+#define __JEFFPC_VAL_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <jeffpc/str.h>
+#include <jeffpc/refcnt.h>
+#include <jeffpc/error.h>
+
+enum val_type {
+	VT_INT = 0,	/* 64-bit uint */
+	VT_STR,		/* a struct str string */
+	VT_SYM,		/* symbol */
+	VT_BOOL,	/* boolean */
+};
+
+struct val {
+	enum val_type type;
+	refcnt_t refcnt;
+	union {
+		uint64_t i;
+		bool b;
+		struct str *str;
+	};
+};
+
+extern void init_val_subsys(void);
+
+extern struct val *val_alloc(enum val_type type);
+extern void val_free(struct val *v);
+extern int val_set_bool(struct val *val, bool v);
+extern int val_set_int(struct val *val, uint64_t v);
+extern int val_set_str(struct val *val, struct str *v);
+extern int val_set_sym(struct val *val, struct str *v);
+extern void val_dump(struct val *v, int indent);
+
+REFCNT_INLINE_FXNS(struct val, val, refcnt, val_free)
+
+#define VAL_ALLOC(t)				\
+	({					\
+		struct val *_x;			\
+		_x = val_alloc(t);		\
+		ASSERT(_x);			\
+		_x;				\
+	})
+
+#define VAL_ALLOC_SYM(v)			\
+	({					\
+		struct val *_x;			\
+		_x = VAL_ALLOC(VT_SYM);		\
+		VAL_SET_SYM(_x, (v));		\
+		_x;				\
+	})
+
+#define VAL_ALLOC_SYM_CSTR(v)			\
+	VAL_ALLOC_SYM(STR_DUP(v))
+
+#define VAL_ALLOC_STR(v)			\
+	({					\
+		struct val *_x;			\
+		_x = VAL_ALLOC(VT_STR);		\
+		VAL_SET_STR(_x, (v));		\
+		_x;				\
+	})
+
+#define VAL_ALLOC_CSTR(v)			\
+	VAL_ALLOC_STR(STR_ALLOC(v))
+
+#define VAL_ALLOC_INT(v)			\
+	({					\
+		struct val *_x;			\
+		_x = VAL_ALLOC(VT_INT);		\
+		VAL_SET_INT(_x, (v));		\
+		_x;				\
+	})
+
+#define VAL_ALLOC_BOOL(v)			\
+	({					\
+		struct val *_x;			\
+		_x = VAL_ALLOC(VT_BOOL);	\
+		VAL_SET_BOOL(_x, (v));		\
+		_x;				\
+	})
+
+#define VAL_SET_INT(val, v)		ASSERT0(val_set_int((val), (v)))
+#define VAL_SET_BOOL(val, v)		ASSERT0(val_set_bool((val), (v)))
+#define VAL_SET_STR(val, v)		ASSERT0(val_set_str((val), (v)))
+#define VAL_SET_SYM(val, v)		ASSERT0(val_set_sym((val), (v)))
+
+#define VAL_DUP_CSTR(v)			VAL_ALLOC_STR(STR_DUP(v))
+
+#endif
---
+++ val.c
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2014-2016 Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <string.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <umem.h>
+
+#include <jeffpc/val.h>
+#include <jeffpc/error.h>
+
+static umem_cache_t *val_cache;
+
+void init_val_subsys(void)
+{
+	val_cache = umem_cache_create("val-cache", sizeof(struct val),
+				      0, NULL, NULL, NULL, NULL, NULL, 0);
+	ASSERT(val_cache);
+}
+
+static void __val_init(struct val *val, enum val_type type)
+{
+	val->type = type;
+
+	switch (type) {
+		case VT_BOOL:
+			val->b = false;
+			break;
+		case VT_INT:
+			val->i = 0;
+			break;
+		case VT_STR:
+		case VT_SYM:
+			val->str = NULL;
+			break;
+	}
+}
+
+static void __val_cleanup(struct val *val)
+{
+	switch (val->type) {
+		case VT_INT:
+		case VT_BOOL:
+			break;
+		case VT_STR:
+		case VT_SYM:
+			str_putref(val->str);
+			break;
+	}
+}
+
+struct val *val_alloc(enum val_type type)
+{
+	struct val *val;
+
+	val = umem_cache_alloc(val_cache, 0);
+	if (!val)
+		return val;
+
+	refcnt_init(&val->refcnt, 1);
+
+	__val_init(val, type);
+
+	return val;
+}
+
+void val_free(struct val *val)
+{
+	ASSERT(val);
+	ASSERT3U(refcnt_read(&val->refcnt), ==, 0);
+
+	__val_cleanup(val);
+
+	umem_cache_free(val_cache, val);
+}
+
+#define DEF_VAL_SET(fxn, vttype, valelem, ctype)		\
+int val_set_##fxn(struct val *val, ctype v)			\
+{								\
+	__val_cleanup(val);					\
+								\
+	val->type = vttype;					\
+	val->valelem = v;					\
+								\
+	return 0;						\
+}
+
+DEF_VAL_SET(int, VT_INT, i, uint64_t)
+DEF_VAL_SET(str, VT_STR, str, struct str *)
+DEF_VAL_SET(sym, VT_SYM, str, struct str *)
+DEF_VAL_SET(bool, VT_BOOL, b, bool)
+
+void val_dump(struct val *val, int indent)
+{
+	if (!val)
+		return;
+
+	switch (val->type) {
+		case VT_STR:
+		case VT_SYM:
+			fprintf(stderr, "%*s'%s'\n", indent, "", str_cstr(val->str));
+			break;
+		case VT_INT:
+			fprintf(stderr, "%*s%"PRIu64"\n", indent, "", val->i);
+			break;
+		case VT_BOOL:
+			fprintf(stderr, "%*s%s", indent, "",
+				val->b ? "true" : "false");
+			break;
+		default:
+			fprintf(stderr, "Unknown type %d\n", val->type);
+			break;
+	}
+}
