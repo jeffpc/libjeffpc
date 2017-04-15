cf6fdb8da42e5b2d5bf71d539436e95a326c91d6
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2017-04-14 23:01:00 -0400

    list: implement illumos-style linked list
    Sometimes, dealing with the Linux kernel-style linked list is a bit hairy.
    For those occasions, we can use the illumos-style list.  We call all the
    functions and macros for this flavor of linked lists "list".
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- include/jeffpc/list.h
+++ include/jeffpc/list.h
@@ -25,6 +25,18 @@
 
 /*
  * Circular doubly-linked list with a sentenel node.
+ *
+ * This file implements two different flavors of the linked list.  The
+ * simpler of the two relies only on struct list_node.  It is up to the user
+ * to know how to convert a struct list_node pointer to a meaningful data
+ * node.  All the functions and macros related to this flavor are of the
+ * form slist_*.
+ *
+ * The second, more complicated one, uses struct list as the "root" of the
+ * list.  It keeps track of the struct list_node offset, and lets the
+ * consumer not worry about the conversion.  It is implemented in terms of
+ * the first flavor.  All the functions and macros related to this flavor
+ * are of the form list_*.
  */
 
 #include <stddef.h>
@@ -36,6 +48,16 @@ struct list_node {
 	struct list_node *next;
 	struct list_node *prev;
 };
+
+struct list {
+	size_t offset;
+	size_t size;
+	struct list_node head;
+};
+
+/*
+ * slist
+ */
 
 static inline void slist_init(struct list_node *node)
 {
@@ -109,5 +131,138 @@ static inline void slist_remove(struct list_node *node)
 	     &pos->member != (list); \
 	     pos = safe, \
 	     safe = slist_entry(safe->member.next, typeof(*pos), member))
+
+/*
+ * list
+ */
+
+extern void list_create(struct list *list, size_t size, size_t offset);
+extern void list_destroy(struct list *list);
+
+/* hepler function - not for consumer use */
+static inline struct list_node *_list_obj2node(struct list *list, void *obj)
+{
+	uintptr_t addr = (uintptr_t) obj;
+
+	return (struct list_node *) (addr + list->offset);
+}
+
+/* hepler function - not for consumer use */
+static inline void *_list_node2obj(struct list *list, struct list_node *node)
+{
+	uintptr_t addr = (uintptr_t) node;
+
+	return (void *) (addr - list->offset);
+}
+
+static inline bool list_is_empty(struct list *list)
+{
+	return slist_is_empty(&list->head);
+}
+
+static inline void list_insert_after(struct list *list, void *ref, void *new)
+{
+	slist_insert_after(_list_obj2node(list, ref),
+			   _list_obj2node(list, new));
+}
+
+static inline void list_insert_before(struct list *list, void *ref, void *new)
+{
+	slist_insert_before(_list_obj2node(list, ref),
+			    _list_obj2node(list, new));
+}
+
+static inline void list_insert_head(struct list *list, void *new)
+{
+	slist_insert_after(&list->head, _list_obj2node(list, new));
+}
+
+static inline void list_insert_tail(struct list *list, void *new)
+{
+	slist_insert_before(&list->head, _list_obj2node(list, new));
+}
+
+/* TODO: is this too big to be a static inline? */
+static inline void *list_next(struct list *list, void *ref)
+{
+	struct list_node *node;
+
+	node = (ref == NULL) ? &list->head : _list_obj2node(list, ref);
+
+	if (node->next == &list->head)
+		return NULL;
+
+	return _list_node2obj(list, node->next);
+}
+
+/* TODO: is this too big to be a static inline? */
+static inline void *list_prev(struct list *list, void *ref)
+{
+	struct list_node *node;
+
+	node = (ref == NULL) ? &list->head : _list_obj2node(list, ref);
+
+	if (node->prev == &list->head)
+		return NULL;
+
+	return _list_node2obj(list, node->prev);
+}
+
+/* TODO: is this too big to be a static inline? */
+static inline void *list_head(struct list *list)
+{
+	return list_next(list, NULL);
+}
+
+/* TODO: is this too big to be a static inline? */
+static inline void *list_tail(struct list *list)
+{
+	return list_prev(list, NULL);
+}
+
+static inline void list_remove(struct list *list, void *item)
+{
+	slist_remove(_list_obj2node(list, item));
+}
+
+/* TODO: is this too big to be a static inline? */
+static inline void *list_remove_head(struct list *list)
+{
+	struct list_node *node;
+
+	if (slist_is_empty(&list->head))
+		return NULL;
+
+	node = slist_head(&list->head);
+
+	slist_remove(node);
+
+	return _list_node2obj(list, node);
+}
+
+/* TODO: is this too big to be a static inline? */
+static inline void *list_remove_tail(struct list *list)
+{
+	struct list_node *node;
+
+	if (slist_is_empty(&list->head))
+		return NULL;
+
+	node = slist_tail(&list->head);
+
+	slist_remove(node);
+
+	return _list_node2obj(list, node);
+}
+
+#define list_for_each(node, list) \
+	for (node = list_head(list); \
+	     node != NULL; \
+	     node = list_next(list, node))
+
+#define list_for_each_safe(node, next, list) \
+	for (node = list_head(list), next = list_next(list, node); \
+	     node != NULL; \
+	     node = next, next = list_next(list, next))
 
 #endif
