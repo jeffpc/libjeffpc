11f0d330313220c90d2c35ae43a609183d8a78dc
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2017-06-08 11:08:28 +0300

    int: refactor endian conversion function generating macros
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- include/jeffpc/int.h
+++ include/jeffpc/int.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
+ * Copyright (c) 2016-2017 Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -69,7 +69,7 @@ STR_TO_INT(64, 0xffffffffffffffffull)
 #define str2u16(s, i)	__str2u16((s), (i), 10)
 
 /*
- * byte ordering
+ * byte swapping
  */
 static inline uint64_t bswap_64(uint64_t in)
 {
@@ -102,31 +102,33 @@ static inline uint8_t bswap_8(uint8_t in)
 	return (in & 0xff);
 }
 
-#define __GEN(from, size, to, bswap)					\
+/*
+ * byte ordering
+ */
+#define ___GEN(from, size, to, bswap)					\
 static inline uint##size##_t from##size##_to_##to(uint##size##_t x)	\
 {									\
 	return bswap;							\
 }
 
 #ifdef CPU_BIG_ENDIAN
-#define GEN(size)							\
-	__GEN(be,  size, cpu, x)					\
-	__GEN(cpu, size, be,  x)					\
-	__GEN(le,  size, cpu, bswap_##size(x))				\
-	__GEN(cpu, size, le,  bswap_##size(x))
+#define __GEN(from, size, to, be, le) ___GEN(from, size, to, be)
 #else
-#define GEN(size)							\
-	__GEN(be,  size, cpu, bswap_##size(x))				\
-	__GEN(cpu, size, be,  bswap_##size(x))				\
-	__GEN(le,  size, cpu, x)					\
-	__GEN(cpu, size, le,  x)
+#define __GEN(from, size, to, be, le) ___GEN(from, size, to, le)
 #endif
+
+#define GEN(size)							\
+	__GEN(be,  size, cpu, x, bswap_##size(x))			\
+	__GEN(cpu, size, be,  x, bswap_##size(x))			\
+	__GEN(le,  size, cpu, bswap_##size(x), x)			\
+	__GEN(cpu, size, le,  bswap_##size(x), x)
 
 GEN(64)
 GEN(32)
 GEN(16)
 GEN(8)
 
+#undef ___GEN
 #undef __GEN
 #undef GEN
 
